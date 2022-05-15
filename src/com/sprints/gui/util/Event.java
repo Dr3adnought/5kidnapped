9f88db594b1728ade381aa63e48dd20f5c19d6f5
KristenCotten <41060883+KristenCotten@users.noreply.github.com>
2022-05-15 11:51:47 -0500

    GUI functionality, started to work on movement with arrows, AB#468, AB#694, AB#625

---
+++ src/com/sprints/gui/util/Event.java
@@ -0,0 +1,24 @@
+package com.sprints.gui.util;
+
+import com.sprints.gui.GameController;
+
+public class Event {
+     GameController gc;
+
+     public Event(GameController gc) {
+          this.gc = gc;
+     }
+
+     //event methods
+
+     public void lookObject(){
+          //TODO
+          //gc.gooey.messageText.setText("display the obj description here");
+     }
+     public void getObject(){
+          //TODO
+     }
+     public void goSomewhere(){
+          //TODO
+     }
+}
