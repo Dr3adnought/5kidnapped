0b4d7a93a91bd71741a6a3ebc8f0351de09c70bc
Jeffrey Lee <dr3adnought@protonmail.com>
2022-05-17 16:36:43 -0400

    Gui volume controls, AB#697

--- src/com/sprints/Game.java
+++ src/com/sprints/Game.java
@@ -141,7 +141,7 @@ public class Game {
         Console.clear();
         Utils.printWithDelays("...You feel a sharp prick.");
         System.out.println();
-        MusicPlayer.playSound("/com/sprints/gui/Sound.wav");
+        MusicPlayer.playSound("/com/sprints/gui/resources/Sound.wav");
     }
 
     // used to display status (current room, inventory, room description, etc)
